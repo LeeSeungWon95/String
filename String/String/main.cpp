dde8cc978da3586de0fe407a66657762d7602717
LEESEUNGWON\Leeseungwon <iseungwon239@gmail.com>
2022-09-27 00:15:46 +0900

    StrCpy 구현

--- String/String/main.cpp
+++ String/String/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 문제 1 문자열 길이를 출력하는 함수
 int StrLen(const char* str)
 {
 	int Count = 0;
@@ -11,12 +12,25 @@ int StrLen(const char* str)
 	}
 	return Count;
 }
+
+// 문제 2 문자열 복사하는 함수
+char* StrCpy(char* dest, char* src)
+{
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
 int main()
 {
 	const int BUF_SIZE = 100;
-	char a[BUF_SIZE] = "Hellowefwwfw";
-	cout << sizeof(a) << endl;
-	cout << strlen(a) << endl;
-	cout << StrLen(a) << endl;
+	char a[BUF_SIZE] = "Hello";
+	char b[BUF_SIZE];
+	cout << StrCpy(b, a) << endl;
+
 	return 0;
 }
