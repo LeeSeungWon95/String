3e998675162124698b5e3181abbd5144816ea767
leeseungwon <free_sstyle@naver.com>
2022-09-27 12:58:50 +0900

    StrCpy, StrCat 포인터 버전

--- String/String/main.cpp
+++ String/String/main.cpp
@@ -12,25 +12,44 @@ int StrLen(const char* str)
 	}
 	return Count;
 }
+#pragma warning(disable : 4996)
 
 // 문제 2 문자열 복사하는 함수
 char* StrCpy(char* dest, char* src)
 {
-	int i = 0;
-	while (src[i] != '\0')
+	while (*src != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		src++;
+		dest++;
 	}
-	dest[i] = '\0';
+	*dest = '\0';
 	return dest;
 }
+
+// 문제 3 문자열 덧붙이는 함수
+char* StrCat(char* dest, char* src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return dest;
+}
+
 int main()
 {
 	const int BUF_SIZE = 100;
 	char a[BUF_SIZE] = "Hello";
-	char b[BUF_SIZE];
-	cout << StrCpy(b, a) << endl;
-
+	char b[BUF_SIZE] = "World";
+	StrCat(a, b);
+	cout << a << endl;
 	return 0;
 }
