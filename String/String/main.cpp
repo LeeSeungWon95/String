395dbd286b1e90663a64c9f5d9913b80d4157e29
leeseungwon <free_sstyle@naver.com>
2022-09-28 10:05:42 +0900

    문자열 뒤집는 함수 구현

--- String/String/main.cpp
+++ String/String/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// 문제 1 문자열 길이를 출력하는 함수
+// 문제1) 문자열 길이를 출력하는 함수
 int StrLen(const char* str)
 {
 	int Count = 0;
@@ -14,7 +14,7 @@ int StrLen(const char* str)
 }
 #pragma warning(disable : 4996)
 
-// 문제 2 문자열 복사하는 함수
+// 문제2) 문자열 복사하는 함수
 char* StrCpy(char* dest, char* src)
 {
 	while (*src != '\0')
@@ -27,7 +27,7 @@ char* StrCpy(char* dest, char* src)
 	return dest;
 }
 
-// 문제 3 문자열 덧붙이는 함수
+// 문제3) 문자열 덧붙이는 함수
 char* StrCat(char* dest, char* src)
 {
 	int Len = StrLen(dest);
@@ -41,7 +41,7 @@ char* StrCat(char* dest, char* src)
 	return dest;
 }
 
-// 문제 4 두 문자열 비교하는 함수
+// 문제4) 두 문자열 비교하는 함수
 int StrCmp(char* a, char* b)
 {
 	int i = 0;
@@ -59,11 +59,26 @@ int StrCmp(char* a, char* b)
 	}
 	return 0;
 }
+
+// 문제5) 문자열을 뒤집는 함수
+void ReverseStr(char* str)
+{
+	int Len = StrLen(str);
+	int i = 0;
+	while (i < Len / 2)
+	{
+		char temp = str[i];
+		str[i] = str[Len - 1 - i];
+		str[Len - 1 - i] = temp;
+		i++;
+	}
+}
+
 int main()
 {
 	const int BUF_SIZE = 100;
-	char a[BUF_SIZE] = "Hfllo";
-	char b[BUF_SIZE] = "Hello";
-	cout << StrCmp(a, b) << endl;
+	char b[BUF_SIZE] = "Hello World";
+	ReverseStr(b);
+	cout << b << endl;
 	return 0;
 }
